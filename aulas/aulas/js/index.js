6584fc933f7b59ca4eaf6617ae4d12627aa2908a
ANDERSON BENTO <108364968+DevKBento@users.noreply.github.com>
2022-07-08 20:23:10 -0300

    Add files via upload

--- aulas/aulas/js/index.js
+++ aulas/aulas/js/index.js
@@ -1,4 +1,19 @@
+let nome = 'Silvia';
+var meu_nome = 'Anderson';
+let k;
+let nomecliente = 'Luiz';
+let nomeCliente = 'Roberto';
+
+// não podemos redeclarar as variaveis let
+
 console.log('Olá Mundo');
 console.log('trecho exibido apenas no console do chrome');
-
+console.log(nome,' nasceu em 1984 ');
+console.log(nome,'casou com ',meu_nome);
 alert('Olá mundo');
+
+console.log(nomecliente);
+console.log(nomeCliente);
+
+nomeCliente = 'Dirce'
+console.log(nomeCliente);
